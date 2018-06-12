d761b625e62c615dd47ce329a4181185e2ea7ef5
Daniel Kirchner <cucu851223@gmail.com>
2018-06-12 22:39:48 +0200

    graphql query with apollo client in react

--- src/App/index.js
+++ src/App/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 
+import Profile from '../Profile';
+
 class App extends Component {
   render() {
-    return <div>I'm the App!</div>;
+    return <Profile />;
   }
 }
 
