c4c95dd6f77f59d27841ba56dd83ab5f06b64d73
Daniel Kirchner <cucu851223@gmail.com>
2018-06-14 16:56:24 +0200

    pagination

--- src/Repository/RepositoryList/index.js
+++ src/Repository/RepositoryList/index.js
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
+import Loading from '../../Loading';
 import RepositoryItem from '../RepositoryItem';
 
-const RepositoryList = ({ repositories }) =>
-  repositories.edges.map(({ node }) => (
-    <div key={node.id} className="RepositoryItem">
-      <RepositoryItem {...node} />
-    </div>
-  ));
+const updateQuery = (previousResult, { fetchMoreResult }) => {
+  if (!fetchMoreResult) {
+    return previousResult;
+  }
+
+  return {
+    ...previousResult,
+    viewer: {
+      ...previousResult.viewer,
+      repositories: {
+        ...previousResult.viewer.repositories,
+        ...fetchMoreResult.viewer.repositories,
+        edges: [
+          ...previousResult.viewer.repositories.edges,
+          ...fetchMoreResult.viewer.repositories.edges
+        ]
+      }
+    }
+  };
+};
+
+const RepositoryList = ({ repositories, loading, fetchMore }) => (
+  <Fragment>
+    {repositories.edges.map(({ node }) => (
+      <div key={node.id} className="RepositoryItem">
+        <RepositoryItem {...node} />
+      </div>
+    ))}
+
+    {loading ? (
+      <Loading />
+    ) : (
+      repositories.pageInfo.hasNextPage && (
+        <button
+          type="button"
+          onClick={() =>
+            fetchMore({
+              variables: {
+                cursor: repositories.pageInfo.endCursor
+              },
+              updateQuery
+            })
+          }
+        >
+          More Repositories
+        </button>
+      )
+    )}
+  </Fragment>
+);
 
 export default RepositoryList;
